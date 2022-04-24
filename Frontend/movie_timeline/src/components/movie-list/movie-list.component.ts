44a5aa87e395689acb162399c7fe428d71f10361
Simone Maffa <simone.maffa23@gmail.com>
2022-04-25 01:17:50 +0200

    add modifiche grafiche

--- Frontend/movie_timeline/src/components/movie-list/movie-list.component.ts
+++ Frontend/movie_timeline/src/components/movie-list/movie-list.component.ts
@@ -8,6 +8,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 })
 export class MovieListComponent implements OnInit {
 
+  user: string = "Nicolas Cage"
   searchIcon = faSearch;
   constructor() { }
 
