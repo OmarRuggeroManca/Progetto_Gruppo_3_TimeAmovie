9cb2f7240fd75026981e8ec56a545efe7c481b66
Omar Ruggero Manca <92058564+OmarRuggeroManca@users.noreply.github.com>
2022-04-29 13:28:32 +0200

    Aggiunti controlli al login form

--- Frontend/movie_timeline/src/components/login/login.component.ts
+++ Frontend/movie_timeline/src/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/models/User';
 import { BackendAPIService } from 'src/services/backend-api.service';
 
 @Component({
@@ -16,20 +17,41 @@ export class LoginComponent implements OnInit {
               private router: Router) { }
 
   usernameOrPasswordWrong: boolean = false;
+  usernameWrong: boolean = false
+  passwordWrong: boolean = false
   
 
   ngOnInit(): void {
   }
 
+  //Metodo alla ricerca di uno scopo
+  /*resetStates(){ 
+    this.usernameOrPasswordWrong = false;
+    this.usernameWrong = false;
+    this.passwordWrong = false;
+  }*/
+
   login(user: NgForm) {
-    this.backendAPIService.postLogin(user.value).subscribe({
-      next: (res) =>{if(res!== null){ 
+    //controllo per campi del form nulli
+    if(user.value.username !== null && user.value.password !== null){ 
+      //Il metodo del login viene richiamato 
+        this.backendAPIService.postLogin(user.value).subscribe({
+        next: (res) =>{if(res!== null){ //Se abbiamo risposta positiva dal server res non è null
         this.backendAPIService.userLogged = true;         
         this.backendAPIService.userActive = res;
-        this.router.navigateByUrl(`/params`);
+        this.router.navigateByUrl(`/params`); //Il metodo rimanda direttamente alla pagina di filtraggio
+        
       }},
-      error: () => this.usernameOrPasswordWrong = true
-    });
+      error: () => this.usernameOrPasswordWrong = true //Se le credenziali sono sbagliate
+
+      });
+    }else{  //Controlli dei campi delle credenziali
+      if(user.value.username === null){
+      this.usernameWrong = true;
+      }if (user.value.password === null){
+         this.passwordWrong = true;
+      }      
+    }    
   } 
 
 
